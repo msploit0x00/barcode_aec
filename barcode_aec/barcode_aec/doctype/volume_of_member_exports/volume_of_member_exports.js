01efc2913679fe950f5ffce2b66a0b698867dcfa
msploit0x00 <minasploit1@outlook.com>
2024-05-30 10:11:47 +0300

    exporting doctypes

---
+++ barcode_aec/barcode_aec/doctype/volume_of_member_exports/volume_of_member_exports.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, ds and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Volume Of Member Exports', {
+	// refresh: function(frm) {
+
+	// }
+});
