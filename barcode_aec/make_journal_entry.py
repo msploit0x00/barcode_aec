e957e36b0a572dfc18f9a27a40d2cf5354d929f2
msploit0x00 <minasploit1@outlook.com>
2024-08-25 16:19:32 +0300

    automating journal entry and mail

--- barcode_aec/make_journal_entry.py
+++ barcode_aec/make_journal_entry.py
@@ -1,59 +1,73 @@
 import frappe
-from frappe import _
-from frappe.utils import now, today
+from frappe.utils import now_datetime, today
 
-@frappe.whitelist()
-def make_journal_entry():
-    docs = frappe.get_all("Treasury bills")
+def cron():
+    print("HIiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii")
     setting1 = frappe.db.get_single_value('Treasury bill setting', 'baio1')
     setting2 = frappe.db.get_single_value('Treasury bill setting', 'baio2')
     setting3 = frappe.db.get_single_value('Treasury bill setting', 'baio3')
     setting4 = frappe.db.get_single_value('Treasury bill setting', 'baio4')
     setting5 = frappe.db.get_single_value('Treasury bill setting', 'due_returns')
     setting6 = frappe.db.get_single_value('Treasury bill setting', 'taxes_owed')
+    # Fetch the single Treasury bill setting document
+    docs = frappe.get_all("Treasury bills")
+    # # # Get the current time in 'HH:MM' format
+    current_time = now_datetime().strftime('%H:%M:%S')
+    print("HIiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii")
+    # # # Extract the time in 'HH:MM' format from doc.time, assuming doc.time is stored as 'HH:MM:SS'
     for doc in docs:
         Treasury_bill = frappe.get_doc("Treasury bills", doc.name)
         table = Treasury_bill.payment_schedule
+        print("Treasury_bill",table)
         for row in table:
-            print(row.entry_date, today())  # Print entry date and current date
+            print("Row",row)  # Print entry date and current date
             if today() == str(row.entry_date):
-                print("Ok.....")
+                print("Dayessss",row.dayes)  # Print entry date and current date
                 journal_entry = frappe.get_doc({
-                    'doctype': 'Journal Entry',
-                    'posting_date': today(),
-                    'accounts': [
-                        {
-                            "account": setting5,
-                            "credit": row.earned_return,
-                            "against_account": setting3,
-                            "credit_in_account_currency": row.earned_return,
-                        },
-                        {
-                            "account": setting3,
-                            "debit": row.earned_return,
-                            "against_account": setting5,
-                            "debit_in_account_currency": row.earned_return,
-                        },
-                        {
-                            "account": setting6,
-                            "credit": row.tax_on_return,
-                            "against_account": setting4,
-                            "credit_in_account_currency": row.tax_on_return,
-                        },
-                        {
-                            "account": setting4,
-                            "debit": row.tax_on_return,
-                            "against_account": setting6,
-                            "debit_in_account_currency": row.tax_on_return,
-                        },
-                    ]
-                })
+                'doctype': 'Journal Entry',
+                'posting_date': today(),
+                'accounts': [
+                    {
+                        "account": setting5,
+                        "credit": row.earned_return,
+                        "against_account": setting3,
+                        "credit_in_account_currency": row.earned_return,
+                    },
+                    {
+                        "account": setting3,
+                        "debit": row.earned_return,
+                        "against_account": setting5,
+                        "debit_in_account_currency": row.earned_return,
+                    },
+                    {
+                        "account": setting6,
+                        "credit": row.tax_on_return,
+                        "against_account": setting4,
+                        "credit_in_account_currency": row.tax_on_return,
+                    },
+                    {
+                        "account": setting4,
+                        "debit": row.tax_on_return,
+                        "against_account": setting6,
+                        "debit_in_account_currency": row.tax_on_return,
+                    },
+                ] })
                 journal_entry.insert()
                 journal_entry.submit()
 
                 # Update the payment schedule row with the journal entry reference
                 frappe.db.set_value('Payment', row.name, 'journal_entry', journal_entry.name)
                 frappe.db.set_value('Treasury bills', doc.name,'make_entry', 1)
+                print("Ok.....")
 
             else:
                 print("Not Ok.....")
+
+
+
+
+
+
+
+
+
---
+++ barcode_aec/sendmail2.py
@@ -0,0 +1,61 @@
+# Copyright (c) 2024, ds and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.utils import now, today
+from datetime import datetime
+
+def send_email():
+    # current Date and time
+    current_date = today()
+    current_time_in_hour_min = datetime.now().strftime('%H:%M') 
+    # get all generalization Queuing
+    # print("current Date and time",current_date,current_time_in_hour_min)
+    docs =frappe.get_all("Generalization Queuing")
+    print("Generalization Queuing",docs)
+    for doc in docs:
+        generalization_queuing = frappe.get_doc("Generalization Queuing", doc.name)
+        total_seconds = generalization_queuing.time.total_seconds()
+        hours = int(total_seconds // 3600)
+        minutes = int((total_seconds % 3600) // 60)
+        doc_time = f"{hours:02}:{minutes:02}"
+        print("Generalization  doc_time ", doc_time)
+
+        if current_date == str(generalization_queuing.date):
+            if current_time_in_hour_min == doc_time:
+                print("yes")
+                recipients = [customer.email for customer in generalization_queuing.customer_email]
+                args = generalization_queuing.as_dict()
+                args["message"] = get_message(generalization_queuing,args)
+
+                email_args = {
+                    "subject":generalization_queuing.subject,
+                    "sender":generalization_queuing.sender_email,
+                    "recipients":recipients,
+                    "attachments":get_attachments(generalization_queuing),
+                    "template": "newsletter",
+                    "reference_doctype": generalization_queuing.doctype,
+                    "reference_name": generalization_queuing.name,
+                    "queue_separately": True,
+                    "send_priority": 0,
+
+                }
+                print("yes2")
+                frappe.sendmail(**email_args)
+                print("yes3")
+                generalization_queuing.status = 'Sent'
+
+                frappe.db.commit()
+
+
+
+                        
+        else:
+            print("current Date and time not matched")    
+ 
+def get_attachments(generalization_queuing):
+    return [{"file_url":row.attachment} for row in generalization_queuing.attachs]
+
+def get_message(generalization_queuing,args):
+    content = frappe.render_template(generalization_queuing.body, {"doc": args})
+    return content	                
