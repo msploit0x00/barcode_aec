23ea34565e6abe99cf8d4cac9600bebf3aa211b7
GrandMasterOfTemple <100480646+msploit0x00@users.noreply.github.com>
2024-09-04 21:38:33 +0300

    fixing email template to issue_email

--- barcode_aec/issue.py
+++ barcode_aec/issue.py
@@ -30,7 +30,7 @@ def send_email(name,print_format_name):
                 "sender": issue.custom_email,
                 "recipients": [contact.email],
                 "attachments": attachments,
-                "template": "issue_mail",
+                "template": "issue_email",
                 "reference_doctype": issue.doctype,
                 "reference_name": name,
                 "queue_separately": True,
