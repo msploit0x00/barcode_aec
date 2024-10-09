6fa5b5c99eedf069614e1ac5f64b6dbb2229a212
GrandMasterOfTemple <100480646+msploit0x00@users.noreply.github.com>
2024-10-09 16:38:55 +0300

    printing journal entry

--- barcode_aec/make_journal_entry.py
+++ barcode_aec/make_journal_entry.py
@@ -57,6 +57,7 @@ def cron():
             journal_entry.submit()
 
                 # Update the payment schedule row with the journal entry reference
+            print(journal_entry)
         frappe.db.set_value('Payment', row.name, 'journal_entry', journal_entry.name)
         print(doc.name)
         frappe.db.set_value('Treasury bills', doc.name,'make_entry', 1)
