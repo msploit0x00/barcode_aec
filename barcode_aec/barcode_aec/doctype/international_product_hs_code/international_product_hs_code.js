7a08db8d39416e9c0406c9a27dacbfdb2ca5786d
msploit0x00 <minasploit1@outlook.com>
2024-10-13 16:48:01 +0300

    exporting docs and custom fields with ahmed radwan el king

---
+++ barcode_aec/barcode_aec/doctype/international_product_hs_code/international_product_hs_code.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, ds and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('International Product HS Code', {
+	// refresh: function(frm) {
+
+	// }
+});
