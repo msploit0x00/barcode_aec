9aa9fcd2402203761a10f98cf96ca0722f2d1cea
OHany1999 <omar1999410@gmail.com>
2025-01-14 14:17:43 +0200

    Add send email buttons

--- barcode_aec/barcode_aec/doctype/company_profile_form/company_profile_form.js
+++ barcode_aec/barcode_aec/doctype/company_profile_form/company_profile_form.js
@@ -1,8 +1,35 @@
 // Copyright (c) 2024, ds and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Company Profile Form', {
-	// refresh: function(frm) {
 
-	// }
+frappe.ui.form.on('Company Profile Form', {
+    send_mail: function(frm) {
+        
+        const sender_mail = frm.doc.email_sender;
+        const subject = frm.doc.subject;
+        const body_mail = frm.doc.body_mail;
+        const email_reciver = frm.doc.email;
+
+        if (!sender_mail || !subject || !body_mail || !email_reciver) {
+            frappe.msgprint(__("Please fill all the required fields (Sender Mail, Subject, Body Mail, and Email Receiver)."));
+            return;
+        }
+
+        frappe.call({
+            method: 'barcode_aec.barcode_aec.doctype.company_profile_form.company_profile_form.send_email',
+            args: {
+                sender_mail: sender_mail,
+                subject: subject,
+                body_mail: body_mail,
+                email_reciver: email_reciver
+            },
+            callback: function(r) {
+                if (r.message) {
+                    frappe.msgprint(__("Email sent successfully!"));
+                } else {
+                    frappe.msgprint(__("Failed to send email. Please try again."));
+                }
+            }
+        });
+    }
 });
--- barcode_aec/barcode_aec/doctype/company_profile_form/company_profile_form.py
+++ barcode_aec/barcode_aec/doctype/company_profile_form/company_profile_form.py
@@ -1,8 +1,25 @@
 # Copyright (c) 2024, ds and contributors
 # For license information, please see license.txt
 
-# import frappe
+
+import frappe
 from frappe.model.document import Document
 
 class CompanyProfileForm(Document):
-	pass
+    pass
+
+
+@frappe.whitelist()
+def send_email(sender_mail, subject, body_mail, email_reciver):
+    try:
+        # Send email using frappe.sendmail
+        frappe.sendmail(
+            sender= sender_mail,
+            recipients= email_reciver,
+            subject= subject,
+            message= body_mail
+        )
+        return "Email sent successfully!"
+    except Exception as e:
+        frappe.log_error(frappe.get_traceback(), "Failed to send email")
+        return f"Failed to send email: {str(e)}"
